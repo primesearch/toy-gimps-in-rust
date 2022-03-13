00f19ecc8738c426ce557d4dec67ddb61d08aa44
Josh McFerran <mcferran@pdx.edu>
2022-03-12 19:00:51 -0800

    Cleaned up some things; added println to ifft for debugging

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::sync;
 /// of the work to prptest
 fn main() {
     let (mut exponent, mut signal_length, mut update_frequency) = (0, 0, 0);
+    let mut run_verbose = false;
     let mut args_list = std::env::args();
     args_list.next();
     while let Some(argument) = args_list.next() {
@@ -28,13 +29,14 @@ fn main() {
                     usage();
                 }
             }
-            "-f" | "-freq" | "--frequency" => {
+            "-f" | "-F" | "-freq" | "--frequency" => {
                 if let Some(next_arg) = args_list.next() {
                     update_frequency = parsenum(next_arg);
                 } else {
                     usage();
                 }
             }
+            "-v" | "-V" | "--verbose" => run_verbose = true,
             &_ => usage(),
         }
     }
@@ -49,7 +51,7 @@ fn main() {
         update_frequency = ((exponent as f64) / 100.0).ceil() as usize;
     }
 
-    if prptest(exponent, signal_length, update_frequency) {
+    if prptest(exponent, signal_length, update_frequency, run_verbose) {
         println!("2 ^ {:?} - 1 is probably prime!", exponent);
     } else {
         println!("2 ^ {:?} - 1 is not prime.", exponent);
@@ -58,11 +60,24 @@ fn main() {
 
 /// This runs the actual loop and is the bulk of the program. Returns true if 2 ^ exponent -1
 /// is probably prime, false otherwise.
-fn prptest(exponent: usize, signal_length: usize, update_frequency: usize) -> bool {
-    let (_bit_array, two_to_the_bit_array, weight_array, fft, ifft) =
-        initialize_constants(exponent, signal_length);
+fn prptest(
+    exponent: usize,
+    signal_length: usize,
+    update_frequency: usize,
+    run_verbose: bool,
+) -> bool {
+    let (bit_array, two_to_the_bit_array) = init_bit_array(exponent, signal_length);
+    let weight_array = init_weight_array(exponent, signal_length);
+    let (fft, ifft) = init_fft(signal_length);
 
     let mut residue = signalize(3, &two_to_the_bit_array);
+
+    if run_verbose {
+        println!(
+            "Bit Array: {:?}\ntwo_to_the_bit_array: {:?}\n",
+            bit_array, two_to_the_bit_array
+        );
+    }
 
     for i in 0..exponent {
         if i % update_frequency == 0 {
@@ -71,12 +86,22 @@ fn prptest(exponent: usize, signal_length: usize, update_frequency: usize) -> bo
             // println!("Current Roundoff Error: {:.4}", roundoff);
             println!("Residue: {:?}\n", &residue);
         }
-        residue = squaremod_with_ibdwt(residue, &two_to_the_bit_array, &weight_array, &fft, &ifft);
+        if run_verbose {
+            println!("Designalized: {:?}", designalize(&residue, &two_to_the_bit_array));
+        }
+        residue = squaremod_with_ibdwt(
+            residue,
+            &two_to_the_bit_array,
+            &weight_array,
+            &fft,
+            &ifft,
+            run_verbose,
+        );
     }
 
     println!("Final Residue: {:?}", residue);
 
-    designalize(residue, &two_to_the_bit_array) == 9
+    designalize(&residue, &two_to_the_bit_array) == 9
 }
 
 fn squaremod_with_ibdwt(
@@ -85,22 +110,39 @@ fn squaremod_with_ibdwt(
     weight_array: &[f64],
     fft: &std::sync::Arc<dyn Fft<f64>>,
     ifft: &std::sync::Arc<dyn Fft<f64>>,
+    run_verbose: bool,
 ) -> Vec<f64> {
     let balanced_signal = balance(signal, two_to_the_bit_array);
+    if run_verbose {
+        println!("Balanced: {:?}", balanced_signal);
+    }
+
     let transformed_signal = weighted_transform(balanced_signal, weight_array, fft);
-
+    if run_verbose {
+        println!("fft: {:?}", transformed_signal);
+    }
+    
     let squared_transformed_signal = transformed_signal.into_iter().map(|x| x * x).collect();
-
+    if run_verbose {
+        println!("Squared fft: {:?}", squared_transformed_signal);
+    }
+    
     let squared_signal = inverse_weighted_transform(squared_transformed_signal, weight_array, ifft);
+    if run_verbose {
+        println!("ifft:  {:?}", squared_signal);
+    }
+
     let rounded_signal = squared_signal.into_iter().map(|x| x.round()).collect();
+    if run_verbose {
+        println!("Rounded:  {:?}\n\n", rounded_signal);
+    }
 
     complete_carry(rounded_signal, two_to_the_bit_array)
 }
 
 fn balance(mut signal: Vec<f64>, two_to_the_bit_array: &[f64]) -> Vec<f64> {
     let mut carry_val = 0.0;
-    let mut i = 0;
-    while i < signal.len() {
+    for i in 0..signal.len() {
         signal[i] += carry_val;
         if signal[i] > two_to_the_bit_array[i] / 2.0 {
             signal[i] -= two_to_the_bit_array[i];
@@ -108,7 +150,6 @@ fn balance(mut signal: Vec<f64>, two_to_the_bit_array: &[f64]) -> Vec<f64> {
         } else {
             carry_val = 0.0;
         }
-        i += 1;
     }
     signal
 }
@@ -120,14 +161,12 @@ fn weighted_transform(
 ) -> Vec<Complex<f64>> {
     let mut complex_signal = Vec::new();
 
-    let mut i = 0;
-    while i < signal.len() {
+    for i in 0..signal.len() {
         signal[i] *= weight_array[i];
         complex_signal.push(Complex {
             re: signal[i],
             im: 0.0,
         });
-        i += 1;
     }
 
     fft.process(&mut complex_signal);
@@ -142,12 +181,11 @@ fn inverse_weighted_transform(
     let mut real_signal = Vec::new();
 
     ifft.process(&mut complex_signal);
+    println!("Unweighted ifft: {:?}", complex_signal);
 
-    let mut i = 0;
-    while i < complex_signal.len() {
+    for i in 0..complex_signal.len() {
         real_signal.push(complex_signal[i].re);
         real_signal[i] /= weight_array[i];
-        i += 1;
     }
 
     real_signal
@@ -156,13 +194,12 @@ fn inverse_weighted_transform(
 fn complete_carry(mut signal: Vec<f64>, two_to_the_bit_array: &[f64]) -> Vec<f64> {
     let mut carry_val = 0.0;
     let signal_length = signal.len();
-    let mut i = 0;
-    while i < signal_length {
+    for i in 0..signal_length {
         signal[i] += carry_val;
         carry_val = (signal[i] / two_to_the_bit_array[i]).floor();
         signal[i] %= two_to_the_bit_array[i];
-        i += 1;
     }
+    let mut i = 0;
     while carry_val != 0.0 {
         signal[i % signal_length] += carry_val;
         carry_val = (signal[i % signal_length] / two_to_the_bit_array[i % signal_length]).floor();
@@ -173,22 +210,12 @@ fn complete_carry(mut signal: Vec<f64>, two_to_the_bit_array: &[f64]) -> Vec<f64
     signal
 }
 
-fn initialize_constants(
-    exponent: usize,
-    signal_length: usize,
-) -> (
-    Vec<f64>,
-    Vec<f64>,
-    Vec<f64>,
-    sync::Arc<dyn Fft<f64>>,
-    sync::Arc<dyn Fft<f64>>,
-) {
+fn init_bit_array(exponent: usize, signal_length: usize) -> (Vec<f64>, Vec<f64>) {
     let fexponent: f64 = exponent as f64;
     let fsignal_length: f64 = signal_length as f64;
-    let mut fi: f64;
-
     let mut bit_array = Vec::new();
-    fi = 1.0;
+
+    let mut fi = 1.0;
     while fi < fsignal_length + 1.0 {
         bit_array.push(
             ((fexponent * fi) / fsignal_length).ceil()
@@ -201,9 +228,15 @@ fn initialize_constants(
     for i in bit_array.iter() {
         two_to_the_bit_array.push(2_f64.powf(*i));
     }
+    (bit_array, two_to_the_bit_array)
+}
 
+fn init_weight_array(exponent: usize, signal_length: usize) -> Vec<f64> {
+    let fexponent: f64 = exponent as f64;
+    let fsignal_length: f64 = signal_length as f64;
     let mut weight_array = Vec::new();
-    fi = 0.0;
+
+    let mut fi = 0.0;
     while fi < fsignal_length {
         weight_array.push(
             2_f64.powf((fexponent * fi) / fsignal_length).ceil()
@@ -211,12 +244,15 @@ fn initialize_constants(
         );
         fi += 1.0;
     }
+    weight_array
+}
 
+fn init_fft(signal_length: usize) -> (sync::Arc<dyn Fft<f64>>, sync::Arc<dyn Fft<f64>>) {
     let mut planner = FftPlanner::<f64>::new();
     let fft = planner.plan_fft_forward(signal_length);
     let ifft = planner.plan_fft_inverse(signal_length);
 
-    (bit_array, two_to_the_bit_array, weight_array, fft, ifft)
+    (fft, ifft)
 }
 
 fn signalize(mut num_to_signalize: usize, two_to_the_bit_array: &[f64]) -> Vec<f64> {
@@ -230,14 +266,12 @@ fn signalize(mut num_to_signalize: usize, two_to_the_bit_array: &[f64]) -> Vec<f
     signal
 }
 
-fn designalize(signal: Vec<f64>, two_to_the_bit_array: &[f64]) -> i64 {
+fn designalize(signal: &[f64], two_to_the_bit_array: &[f64]) -> i64 {
     let mut base = 1;
     let mut resultant_num = 0;
-    let mut i = 0;
-    while i < signal.len() {
+    for i in 0..signal.len() {
         resultant_num += signal[i] as i64 * base;
         base *= two_to_the_bit_array[i] as i64;
-        i += 1;
     }
     resultant_num
 }
